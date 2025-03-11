6d36911a191aad7feb9bcbb7c875d6a1443255fc
eltonpds <eltonpereira505@gmail.com>
2025-03-11 13:36:10 -0300

    Animação durante scroll

---
+++ _js/animations.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {    
+    if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $(window).scroll( function(){   
+            $('.hideme').each( function(i){            
+                var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+                
+                if( bottom_of_window > bottom_of_object ){
+                    
+                    $(this).animate({'opacity':'1'},250);                    
+                }            
+            });     
+        });  
+      } 
+      else {
+        $('.hideme').animate({'opacity':'1'},0);
+      }
+});
+
