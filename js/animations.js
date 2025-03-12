2ac356725a2fdc3fdec2bd3d19c8a54884990ce4
eltonpds <eltonpereira505@gmail.com>
2025-03-12 11:16:13 -0300

    Alterado animação e css para utilizar fadeIn invés do animate

--- js/animations.js
+++ js/animations.js
@@ -1,27 +1,20 @@
 $(document).ready(function() {    
-    if(navigator.userAgent.match(/Android/i) === false
-        || navigator.userAgent.match(/webOS/i) === false
-        || navigator.userAgent.match(/AppleWebKit/i) === false
-        || navigator.userAgent.match(/Mobile Safari/i) === false
-        || navigator.userAgent.match(/iPhone/i) === false
-        || navigator.userAgent.match(/iPad/i) === false
-        || navigator.userAgent.match(/iPod/i) === false
-        || navigator.userAgent.match(/BlackBerry/i) === false 
-        || navigator.userAgent.match(/Windows Phone/i) === false) {
-        $(window).scroll( function(){   
-           $('.hideme').each( function(i){            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);                    
+    if(navigator.userAgent.match(/Windows/i) !== null
+    && navigator.userAgent.match(/Android/i) === null) {
+      $(window).scroll( function(){   
+      $('.hideme').each( function(i){            
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > bottom_of_object ){ 
+                $(this).fadeIn(2000);   
             }            
         });     
         });  
       } 
-      else {
-        $('.hideme').fadeTo(1000, 1);
+      else  if (navigator.userAgent.match(/Windows/i) === null 
+      && navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/Nexus/i)) {
+        $('.hideme').show();
       }
 });
-
